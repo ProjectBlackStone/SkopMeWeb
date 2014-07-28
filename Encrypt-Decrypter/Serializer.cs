9c28e596fb21025ee25fe1f8f0d9d7ef04f70f55
ashwin <ashwin_v_s@hotmail.com>
2014-07-28 21:24:12 +0200

    asdas

--- Encrypt-Decrypter/Serializer.cs
+++ Encrypt-Decrypter/Serializer.cs
@@ -11,7 +11,9 @@ namespace Encrypt_Decrypter
     {
         public string SerializeClass()
         {
-            return new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(new RegisterModel() { UserName = "ashwin", Password = "pass" });
+            //return new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(new RegisterModel() { UserName = "ashwin", Password = "pass" });
+
+            return new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(new LoginModel() { UserName = "ashwin", Password = "pass" , RememberMe = false});
         }
 
     }
--- SkopMe.WebApi/Controllers/ValuesController.cs
+++ SkopMe.WebApi/Controllers/ValuesController.cs
@@ -4,19 +4,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 using Thinktecture.IdentityModel.Authorization.Mvc;
 
 namespace SkopMe.WebApi.Controllers
 {
-    [ClaimsAuthorize]
     [TokenValidationFilter]
     public class ValuesController : ApiController
     {
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "id:value1", "content:value2" };            
+        }
+
+        // GET api/values
+        [HttpGet]
+        public HttpResponseMessage Test()
+        {
+            HttpResponseMessage msg = new HttpResponseMessage { Content = new StringContent(@"{""id"":1,""content"":""Test1""}", System.Text.Encoding.UTF8, "application/json")   };
+            msg.Headers.Add("Authorization-Token", "ACBCCC");
+            return msg;
+            //return new string[] { "id:value1", "content:value2" };
+            //return @"{""id"":1,""content"":""Test1""}";
         }
 
         // GET api/values/5

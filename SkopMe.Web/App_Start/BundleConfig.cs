ca9e020b72413494a3a79796b525782d7263b371
ashwin <ashwin_v_s@hotmail.com>
2014-09-22 20:00:30 +0200

    asdasdsa

--- SkopMe.Web/App_Start/BundleConfig.cs
+++ SkopMe.Web/App_Start/BundleConfig.cs
@@ -8,20 +8,40 @@ namespace SkopMe.Web
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+            //            "~/Scripts/jquery-ui-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.unobtrusive*",
+            //            "~/Scripts/jquery.validate*"));
+
+             bundles.Add(new ScriptBundle("~/bundles/jquery/unitybootstrap/jquery").Include(
+                        "~/Content/themes/unifybootstrap/assets/plugins/jquery-1.10.2.min.js",
+                        "~/Content/themes/unifybootstrap/assets/plugins/jquery-migrate-1.2.1.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery/unitybootstrap").Include(
+                        "~/Content/themes/unifybootstrap/plugins/bootstrap/js/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery/unitybootstrap/plugins").Include(
+                        "~/Content/themes/unifybootstrap/assets/plugins/back-to-top.js",
+                        "~/Content/themes/unifybootstrap/plugins/flexslider/jquery.flexslider-min.js",
+                        "~/Content/themes/unifybootstrap/plugins/parallax-slider/jquery.cslider.js",
+                        "~/Content/themes/unifybootstrap/plugins/parallax-slider/modernizr.js",
+                        "~/Content/themes/unifybootstrap/plugins/parallax-slider/jquery.cslider.js",
+                        "~/Content/themes/unifybootstrap/plugins/js/app.js",
+                        "~/Content/themes/unifybootstrap/plugins/js/index.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery/IE8").Include(
+                       "~/Content/themes/unifybootstrap/assets/plugins/respond.js",
+                       "~/Content/themes/unifybootstrap/assets/plugins/html5shiv.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
@@ -38,6 +58,16 @@ namespace SkopMe.Web
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/unifybootstrap").Include
+                ("~/Content/themes/unifybootstrap/plugins/bootstrap/css/bootstrap.css",
+                "~/Content/themes/unifybootstrap/assets/style.css",
+                "~/Content/themes/unifybootstrap/plugins/line-icons/line-icons.css",
+                "~/Content/themes/unifybootstrap/plugins/font-awesome/css/font-awesome.css",
+                "~/Content/themes/unifybootstrap/plugins/flexslider/flexslider.css",
+                "~/Content/themes/unifybootstrap/plugins/parallax-slider/parallax-slider.css",
+                "~/Content/themes/unifybootstrap/plugins/default.css"
+                ));
         }
     }
 }
